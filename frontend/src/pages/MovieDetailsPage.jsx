5483099ec0ba5d9cd276d1f10fbcc679c607301e
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-11-15 14:37:26 +0100

    Individual movie page fetches movie correctly

--- frontend/src/pages/MovieDetailsPage.jsx
+++ frontend/src/pages/MovieDetailsPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const MovieDetailsPage = () => {
+  const [movie, setMovie] = useState([]);
+
+  const { id } = useParams();
+
+  const backendURL = "http://localhost:5005";
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    axios
+      .get(`${backendURL}/movies/${id}`, { cancelToken: source.token })
+      .then((response) => response.data)
+      .then((data) => setMovie(data[0]));
+
+    return () => {
+      source.cancel("Component got unmounted");
+    };
+  }, []);
+
+  console.log(movie);
+
   return (
     <div>
-      
+      {movie.title}
     </div>
   );
 }
